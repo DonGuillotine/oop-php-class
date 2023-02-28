06c7492ade6849ee7aa16c7649b7e194d18862ab
Donald Nwokoro <infect3dlab@gmail.com>
2023-02-28 12:07:59 +0100

    PHP Access Modifiers Public, Protected, Private

--- access_modifiers.php
+++ access_modifiers.php
@@ -9,4 +9,28 @@
     $rice->name = "Jollof Rice";
     $rice->color = "Red";
     $rice->weight = "50";
+
+
+    class Drink{
+        public $name;
+        public $category;
+        public $brand;
+
+        function set_name($name){
+            $this->name = $name;
+        }
+
+        protected function set_category($name){
+            $this->color = $name;
+        }
+
+        private function set_brand($name){
+            $this->brand = $name;
+        }
+    }
+
+    $tea = new Drink();
+    $tea->set_name('Green Tea');
+    $tea->set_category('Beverage');
+    $tea->set_brand('Lipton');
 ?>
