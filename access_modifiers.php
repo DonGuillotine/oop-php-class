be244fcc172bcd000281a2fca073bffea6d8fc60
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-01 10:46:52 +0100

    PHP Inheritance I

--- access_modifiers.php
+++ access_modifiers.php
@@ -1,36 +1,36 @@
 <?php 
-    class Food {
-        public $name;
-        protected $color;
-        private $weight;
-    }
+    // class Food {
+    //     public $name;
+    //     protected $color;
+    //     private $weight;
+    // }
 
-    $rice = new Food();
-    $rice->name = "Jollof Rice";
-    $rice->color = "Red";
-    $rice->weight = "50";
+    // $rice = new Food();
+    // $rice->name = "Jollof Rice";
+    // $rice->color = "Red";
+    // $rice->weight = "50";
 
 
-    class Drink{
-        public $name;
-        public $category;
-        public $brand;
+    // class Drink{
+    //     public $name;
+    //     public $category;
+    //     public $brand;
 
-        function set_name($name){
-            $this->name = $name;
-        }
+    //     function set_name($name){
+    //         $this->name = $name;
+    //     }
 
-        protected function set_category($name){
-            $this->color = $name;
-        }
+    //     protected function set_category($name){
+    //         $this->color = $name;
+    //     }
 
-        private function set_brand($name){
-            $this->brand = $name;
-        }
-    }
+    //     private function set_brand($name){
+    //         $this->brand = $name;
+    //     }
+    // }
 
-    $tea = new Drink();
-    $tea->set_name('Green Tea');
-    $tea->set_category('Beverage');
-    $tea->set_brand('Lipton');
+    // $tea = new Drink();
+    // $tea->set_name('Green Tea');
+    // $tea->set_category('Beverage');
+    // $tea->set_brand('Lipton');
 ?>
