fc42bb4f8da6ec02e763bb432d1d73637cfb1194
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-03 11:56:34 +0100

    PHP Class Abstract Classes V

---
+++ abstract_classes_coding_challange.php
@@ -0,0 +1,6 @@
+<?php 
+/* - Create an Abstarct class.
+    - The parent class's abstract method should have one argument
+    - The child class has two optional arguments that are not defined in the parent's abstract method
+*/
+?>
