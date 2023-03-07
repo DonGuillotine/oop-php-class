e35565c0b8388b6360f8a130c949db08d3dd9f19
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-07 11:42:28 +0100

    PHP Traits III

--- traits.php
+++ traits.php
@@ -19,4 +19,28 @@
             echo "Never give up on PHP";
         }
     }
+
+    trait quote_two{
+        public function message_second(){
+            echo "Eat and Drink PHP";
+        }
+    }
+
+    class QuoteMaker{
+        // All Methods in the trait will be available in this clas
+        use quote_one;
+    }
+
+    class QuoteMakerSecond{
+        // All Methods in the trait will be available in this clas
+        use quote_one, quote_two;
+    }
+
+    $quotes = new QuoteMaker();
+    $quotes->message_first();
+    echo "<br>";
+
+    $quotes_two = new QuoteMakerSecond();
+    $quotes_two->message_first();
+    $quotes_two->message_second();
 ?>
