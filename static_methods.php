982f889091fc829637cc3bfb07be1599424d7474
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-07 12:16:34 +0100

    PHP Static Methods II

--- static_methods.php
+++ static_methods.php
@@ -8,4 +8,18 @@
     // Class instanciation not required
     // Call static method
     Wakanda::welcome();
+
+    class Donald{
+        public static function my_name(){
+            echo "Hey there I'm Donald";
+        }
+
+        public function __construct()
+        {
+            // A static method can be accessed from a method in the same class using the self keyword and double colon (::)
+            self::my_name();
+        }
+    }
+
+    new Donald();
 ?>
