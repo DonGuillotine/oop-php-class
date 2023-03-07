4e338fb94f1c2b06e101099b7e106645881dcc5e
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-07 12:10:40 +0100

    PHP Static Methods

---
+++ static_methods.php
@@ -0,0 +1,10 @@
+<?php 
+    class Wakanda{
+        public static function welcome(){
+            echo "Welcome to Wakanda";
+        }
+    }
+
+    // Call static method
+    Wakanda::welcome();
+?>
