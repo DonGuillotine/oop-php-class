a7c213ade7ead94884a431a3911160bd0ad605ff
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-10 10:44:27 +0100

    PHP Static Properties III

--- static_properties.php
+++ static_properties.php
@@ -10,10 +10,24 @@ class Pi{
     public static $value = 3.142;
     public function staticValue()
     {
+        // A static property can be accessed from a method in the same class using the self keyword and double colon (::)
         return self::$value;
     }
 }
 
 $pi = new Pi();
+echo "<br>";
 echo $pi->staticValue();
+
+
+class Money{
+    public static $value = 900;
+}
+
+class Naira extends Money{
+    public function useless(){
+        // To call a static property from a child class, use the parent keyword inside the child class
+        return parent::$value;
+    }
+}
 ?>
