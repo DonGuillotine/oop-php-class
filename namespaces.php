f589bca58d69e0c216650fe33005f7ce3b286805
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-10 11:22:08 +0100

    PHP Namespaces III

--- namespaces.php
+++ namespaces.php
@@ -10,20 +10,12 @@
     }
   }
 
-  $table = new Table();
-  $table->title = "My Table";
-  $table->numRows = 5;
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $table->message();?></title>
-</head>
-<body>
-    <?php $table->message(); ?>
-</body>
-</html>
+  class Row{
+    public $numCells = 0;
+    public function message()
+    {
+        echo "<p>The row has $this->numCells cells.</p>";
+    }
+  }
+?>
