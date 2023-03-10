340b91e54dadb501716743327bc4d6e243fedc35
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-10 10:36:04 +0100

    PHP Static Properties II

--- static_properties.php
+++ static_properties.php
@@ -4,4 +4,16 @@ class SimpleInterest{
 }
 
 echo SimpleInterest::$value;
+
+
+class Pi{
+    public static $value = 3.142;
+    public function staticValue()
+    {
+        return self::$value;
+    }
+}
+
+$pi = new Pi();
+echo $pi->staticValue();
 ?>
